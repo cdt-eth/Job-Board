caae27573b878f63a2313e932e28aba98849665c
christian <christiandavidphoto@gmail.com>
2018-05-16 14:20:30 -0400

    admin db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_181620) do
+ActiveRecord::Schema.define(version: 2018_05_16_182005) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_05_16_181620) do
     t.string "card_exp_month"
     t.string "card_exp_year"
     t.datetime "expires_at"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
