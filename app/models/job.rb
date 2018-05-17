2060a86fee3d0ef52e73f71e4c25ab50332ab546
christian <christiandavidphoto@gmail.com>
2018-05-17 15:56:00 -0400

    title validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,8 @@
 class Job < ApplicationRecord
-    belongs_to :user
-    mount_uploader :avatar, AvatarUploader
-  
-    JOB_TYPES = ["Full-time", "Part-time", "Contract", "Freelance"]
-  end
+  belongs_to :user
+  mount_uploader :avatar, AvatarUploader
+
+  JOB_TYPES = ["Full-time", "Part-time", "Contract", "Freelance"]
+
+  validates :title, presence: true
+end
