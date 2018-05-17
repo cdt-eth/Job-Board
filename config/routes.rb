b43ed420d7ef10d52a0df7e15b29339ff9c88c31
christian <christiandavidphoto@gmail.com>
2018-05-16 20:05:38 -0400

    form scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :jobs
   devise_for :users
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_182005) do
+ActiveRecord::Schema.define(version: 2018_05_17_000125) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "url"
+    t.string "job_type"
+    t.string "location"
+    t.string "job_author"
+    t.boolean "remote_ok"
+    t.string "apply_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
