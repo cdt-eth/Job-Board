6f774faf6115d11d3b6c8db826dd53f520ee22fc
christian <christiandavidphoto@gmail.com>
2018-05-17 15:40:03 -0400

    index controller tweak to actually filter

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,7 +5,11 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.json
   def index
-    @jobs = Job.all.order("created_at desc")
+    if (params.has_key?(:job_type))
+      @jobs = Job.where(job_type: params[:job_type]).order("created_at desc")
+    else
+      @jobs = Job.all.order("created_at desc")
+    end
   end
 
   # GET /jobs/1
