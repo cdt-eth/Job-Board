31620bf22a769283b845240f6aadf073d0694b04
christian <christiandavidphoto@gmail.com>
2018-05-16 20:07:54 -0400

    avatar colum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_17_000125) do
+ActiveRecord::Schema.define(version: 2018_05_17_000727) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_05_17_000125) do
     t.string "apply_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   create_table "users", force: :cascade do |t|
