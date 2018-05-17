73847b67e9966ba773bdedea6a0c74e199494596
christian <christiandavidphoto@gmail.com>
2018-05-17 09:59:28 -0400

    reroute home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-require 'sidekiq/web'
+require "sidekiq/web"
 
 Rails.application.routes.draw do
   resources :jobs
   devise_for :users
-  root to: 'home#index'
+  root to: "jobs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
