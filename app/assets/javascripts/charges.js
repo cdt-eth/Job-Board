d779ad3112fda708f7fda55a609b875a868c812e
christian <christiandavidphoto@gmail.com>
2018-05-19 15:07:02 -0500

    harmony true

--- app/assets/javascripts/charges.js
+++ app/assets/javascripts/charges.js
@@ -1,9 +1,9 @@
 document.addEventListener('turbolinks:load', function() {
-  var public_key = document.querySelector("meta[name='stripe-public-key']").content;
-  var stripe = Stripe(public_key);
-  var elements = stripe.elements();
+  const public_key = document.querySelector("meta[name='stripe-public-key']").content;
+  const stripe = Stripe(public_key);
+  const elements = stripe.elements();
 
-  var style = {
+  const style = {
     base: {
       color: '#32325d',
       lineHeight: '32px',
@@ -20,12 +20,12 @@ document.addEventListener('turbolinks:load', function() {
     }
   };
 
-  var card = elements.create('card', { style: style });
+  const card = elements.create('card', { style: style });
 
   card.mount('#card-element');
 
   card.addEventListener('change', ({ error }) => {
-    var displayError = document.getElementById('card-errors');
+    const displayError = document.getElementById('card-errors');
     if (error) {
       displayError.textContent = error.message;
     } else {
@@ -33,15 +33,15 @@ document.addEventListener('turbolinks:load', function() {
     }
   });
 
-  var form = document.getElementById('new_job');
+  const form = document.getElementById('new_job');
   form.addEventListener('submit', async event => {
     event.preventDefault();
 
-    var { token, error } = await stripe.createToken(card);
+    const { token, error } = await stripe.createToken(card);
 
     if (error) {
       // Inform the customer that there was an error.
-      var errorElement = document.getElementById('card-errors');
+      const errorElement = document.getElementById('card-errors');
       errorElement.textContent = error.message;
     } else {
       // Send the token to your server.
@@ -49,10 +49,10 @@ document.addEventListener('turbolinks:load', function() {
     }
   });
 
-  var stripeTokenHandler = token => {
+  const stripeTokenHandler = token => {
     // Insert the token ID into the form so it gets submitted to the server
-    var form = document.getElementById('new_job');
-    var hiddenInput = document.createElement('input');
+    const form = document.getElementById('new_job');
+    const hiddenInput = document.createElement('input');
     hiddenInput.setAttribute('type', 'hidden');
     hiddenInput.setAttribute('name', 'stripeToken');
     hiddenInput.setAttribute('value', token.id);
